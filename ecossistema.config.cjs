9da8eb7b4e5c8aff93fb4c9aa8fbab6749f3cdc4
thgoas <thgas95@gmail.com>
2024-12-06 10:11:13 -0300

    feat: add config for pm2

---
+++ ecossistema.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+        {
+            name: 'EstoqueMinimalista',
+            port: 3003,
+            exec_mode: 'cluster',
+            instances: 'max',
+            script:'./.output/server/index.mjs'
+        },
+    ],
+}
